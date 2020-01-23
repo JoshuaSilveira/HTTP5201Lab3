44f03599997233731e97ebb0805d8e8c7ae7d87f
Joshua Silveira <43301701+JoshuaSilveira@users.noreply.github.com>
2020-01-23 10:58:51 -0500

    Update Lab-3-unittest-function.js
    checkHumbrid function

--- Lab-3-unittest-function.js
+++ Lab-3-unittest-function.js
@@ -6,3 +6,16 @@ alert("lab 3 Function");//please delete this line once connected.
  * Returns true if input validates.
  * @param {string} idIn
  */
+
+ function checkHumbrId(idIn){
+     "user strict"
+     let validID = false;
+     if(idIn===undefined){
+         return false;
+     }else if(idIn.charAt(0)!=='n'||idIn.charAt(0)!=='N'){
+         return false;
+     }else if((idIn.slice(1,idIn.length)).length!==8){
+        return false;
+     }
+     return true;
+ }
