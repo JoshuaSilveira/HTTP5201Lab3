d79aec1c22bdfe9950580e7ea3a6c089a9262fcd
Sam Bebenek <sam.bebenek@gmail.com>
2020-01-23 11:29:57 -0500

    Added style.css file, completed unittest-test.js file

--- Lab-3-unittest-test.js
+++ Lab-3-unittest-test.js
@@ -1,4 +1,27 @@
 //LAB 3 - UNIT TESTING
 //======== THIS FILE IS FOR THE UNIT TEST ==========
-alert("lab 3 UNIT TEST");//please delete this line once connected.
+//alert("lab 3 UNIT TEST");//please delete this line once connected.
+
+function test__checkHumberId(valueIn, expected) {
+    "use strict";
+
+    let result = checkHumbrId(valueIn);
+    let passOrFail;
+    if(result === expected) {
+        passOrFail = "pass";
+    }
+    else passOrFail = "fail";
+
+    let resultMessage;
+    if(passOrFail === "pass")
+    {
+        resultMessage = "--PASSED--";
+    }
+    else resultMessage = "xxFAILEDxx";
+
+    let message = "Value: " + valueIn + " | Expected: " +  expected + " | Result: <strong>" + result + "<span class=\"" + passOrFail + "\"> " + resultMessage + "</span></strong><br />";
+    document.getElementById("data").innerHTML += message;   
+}
+
+///(n|N)\d{8}/
 
